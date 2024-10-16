1d808f90d9c56e3370a875f7ee354c1f7fd788f8
Adithya Ekanayaka <adithyasean@outlook.com>
2024-10-17 02:43:37 +0530

    postgres password decription to match user input and refactoring

--- src/postgres/server.js
+++ src/postgres/server.js
@@ -1,8 +1,12 @@
 import express from 'express';
-import pool from './db.js'; // Adjust the path to your db.js file
+import cors from 'cors';
 import bodyParser from 'body-parser';
-import cors from 'cors'; // Import the cors package
-import bcrypt from 'bcryptjs';
+import dotenv from 'dotenv';
+import signupRoute from './routes/useSignUpWithEmailAndPassword';
+import loginRoute from './routes/useLogin';
+
+// Load environment variables from .env file
+dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -11,30 +15,9 @@ const PORT = process.env.PORT || 5000;
 app.use(cors()); // Enable CORS for all routes
 app.use(bodyParser.json());
 
-// Endpoint to register a user
-app.post('/api/signup', async (req, res) => {
-    const { uid, email, username, fullName, bio, profilePicURL, followers, following, posts, createdAt, password } = req.body;
-
-    try {
-        // Check if password is provided
-        if (!password) {
-            return res.status(400).json({ error: 'Password is required' });
-        }
-        // Hash the password before saving it to PostgreSQL
-        const hashedPassword = await bcrypt.hash(password, 10);
-        // Convert createdAt from Unix timestamp (milliseconds) to a PostgreSQL-compatible date
-        const createdAtDate = new Date(createdAt).toISOString();
-
-        const result = await pool.query(
-            'INSERT INTO users (uid, email, username, password, full_name, bio, profile_pic_url, followers, following, posts, created_at) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11)',
-            [uid, email, username, hashedPassword, fullName, bio, profilePicURL, followers, following, posts, createdAtDate]
-        );
-        res.status(201).json({ message: 'User registered successfully', result });
-    } catch (error) {
-        console.error('Error saving user to PostgreSQL:', error);
-        res.status(500).json({ error: 'Database error', details: error.message });
-    }
-});
+// routes
+app.use('/api/signup', signupRoute);
+app.use('/api/login', loginRoute);
 
 // Start the server
 app.listen(PORT, () => {

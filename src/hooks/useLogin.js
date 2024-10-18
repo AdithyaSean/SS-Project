728757f81c4e26f9ed1702757ee465e3337844ad
adithyasean <adithyasean@outlook.com>
2024-10-18 22:47:24 +0530

    Refactor useLogin.js to include fetching user data from PostgreSQL after successful login

--- src/hooks/useLogin.js
+++ src/hooks/useLogin.js
@@ -1,3 +1,4 @@
+import axios from 'axios';
 import { useSignInWithEmailAndPassword } from "react-firebase-hooks/auth";
 import useShowToast from "./useShowToast";
 import { auth, firestore } from "../firebase/firebase";
@@ -5,29 +6,39 @@ import { doc, getDoc } from "firebase/firestore";
 import useAuthStore from "../store/authStore";
 
 const useLogin = () => {
-	const showToast = useShowToast();
-	const [signInWithEmailAndPassword, , loading, error] = useSignInWithEmailAndPassword(auth);
-	const loginUser = useAuthStore((state) => state.login);
+    const showToast = useShowToast();
+    const [signInWithEmailAndPassword, , loading, error] = useSignInWithEmailAndPassword(auth);
+    const loginUser = useAuthStore((state) => state.login);
 
-	const login = async (inputs) => {
-		if (!inputs.email || !inputs.password) {
-			return showToast("Error", "Please fill all the fields", "error");
-		}
-		try {
-			const userCred = await signInWithEmailAndPassword(inputs.email, inputs.password);
+    const login = async (inputs) => {
+        if (!inputs.email || !inputs.password) {
+            return showToast("Error", "Please fill all the fields", "error");
+        }
+        try {
+            const userCred = await signInWithEmailAndPassword(inputs.email, inputs.password);
 
-			if (userCred) {
-				const docRef = doc(firestore, "users", userCred.user.uid);
-				const docSnap = await getDoc(docRef);
-				localStorage.setItem("user-info", JSON.stringify(docSnap.data()));
-				loginUser(docSnap.data());
-			}
-		} catch (error) {
-			showToast("Error", error.message, "error");
-		}
-	};
+            if (userCred) {
+                const docRef = doc(firestore, "users", userCred.user.uid);
+                const docSnap = await getDoc(docRef);
+                localStorage.setItem("user-info", JSON.stringify(docSnap.data()));
+                loginUser(docSnap.data());
 
-	return { loading, error, login };
+                // Fetch user data from PostgreSQL
+                const response = await axios.post('http://localhost:5033/api/login', {
+                    uid: userCred.user.uid,
+                });
+
+                if (response.data) {
+                    localStorage.setItem("user-info", JSON.stringify(response.data));
+                    loginUser(response.data);
+                }
+            }
+        } catch (error) {
+            showToast("Error", error.message, "error");
+        }
+    };
+
+    return { loading, error, login };
 };
 
 export default useLogin;

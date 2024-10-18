4ab8ae2f08fcf40d07caa3449c341002312f97c6
adithyasean <adithyasean@outlook.com>
2024-10-18 21:33:28 +0530

    Refactor database configuration and connection handling

--- src/postgres/server.js
+++ src/postgres/server.js
@@ -1,11 +1,22 @@
 import express from 'express';
 import cors from 'cors';
+import pkg from 'pg';
 import bodyParser from 'body-parser';
 import dotenv from 'dotenv';
 import signupRouter from './routes/signUp.js';
 
 // Load environment variables from .env file
 dotenv.config();
+
+const { Pool } = pkg;
+
+const pool = new Pool({
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    database: process.env.DB_DATABASE,
+    password: process.env.DB_PASSWORD,
+    port: process.env.DB_PORT,
+});
 
 const app = express();
 const PORT = process.env.PORT;
